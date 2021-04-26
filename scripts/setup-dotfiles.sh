b2408d5a565bcdd79d106f5f6e895415610deaf0
Batoshu <github@batoshu.me>
2021-04-27 00:33:13 +0200

    Add setup script

---
+++ scripts/setup-dotfiles.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Simple script to setup all dotfiles into system.
+# 
+# Usage: [VERBOSE=(true|false)] setup-dotfiles.sh <repo> [target_home]
+
+echov () {
+	if [ "${VERBOSE}" = true ]; then
+		echo $@
+	fi
+}
+
+if [ $# -eq 1 ]; then
+	SOURCE=$1
+	TARGET=$HOME
+elif [ $# -eq 2 ]; then
+	SOURCE=$1
+	TARGET=$2
+else
+	echo "Invalid arguments."
+	echo "Usage: $0 <repo> [target_home]"
+fi
+
+echo "Source directory: $SOURCE"
+echo "Target directory: $TARGET"
+
+# ~/.config dirs:
+CONFIGS="alacritty autostart fontconfig gtk-3.0 htop kanshi mako minidlna nwg-launchers sway swaylock waybar"
+
+# Remove previous config files
+echov "Removing $TARGET/.zshrc"
+rm -rf $TARGET/.zshrc
+echov "Removing $TARGET/.local/share/applications/"
+rm -rf $TARGET/.local/share/applications
+
+for D in $CONFIGS
+do
+	echov "Removing $TARGET/.config/$D/"
+	rm -rf $TARGET/.config/$D
+done
+
+# Create hard links
+echov "Creating symlink $TARGET/.zshrc"
+ln -sf $SOURCE/.zshrc $TARGET/.zshrc
+echov "Creating symlink $TARGET/.local/share/applications/"
+mkdir -p $TARGET/.local/share
+ln -sf $SOURCE/applications $TARGET/.local/share/applications
+mkdir -p $TARGET/.config
+
+for D in $CONFIGS
+do
+	echov "Creating symlink $TARGET/.config/$D/"
+	ln -sf $SOURCE/$D $TARGET/.config/$D
+done
+
+echo "Done. Have a nice day ^-^"
