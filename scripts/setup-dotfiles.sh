aa3101949bd60b52a956922aff5b623fb1df51bc
Batoshu <github@batoshu.me>
2021-05-14 17:29:19 +0200

    Remove htop config from setup script

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -26,7 +26,7 @@ echo "Source directory: $SOURCE"
 echo "Target directory: $TARGET"
 
 # ~/.config dirs:
-CONFIGS="alacritty autostart fontconfig gtk-3.0 htop kanshi mako minidlna neofetch nwg-launchers ranger sway swaylock udiskie waybar"
+CONFIGS="alacritty autostart fontconfig gtk-3.0 kanshi mako minidlna neofetch nwg-launchers ranger sway swaylock udiskie waybar"
 
 # Remove previous config files
 echov "Removing $TARGET/.zshrc"
